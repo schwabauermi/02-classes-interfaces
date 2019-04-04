6130ad8807578438ad4fd5e24c0f723d15e630ec
schwabauermi <schwabauer.mi@gmail.com>
2019-04-04 11:13:25 +0200

    loesung

--- src/main/java/de/thro/inf/prg3/a02/SimpleListImpl.java
+++ src/main/java/de/thro/inf/prg3/a02/SimpleListImpl.java
@@ -1,11 +1,74 @@
 package de.thro.inf.prg3.a02;
+import java.util.Iterator;
 
 /**
  * @author Peter Kurfer
  * Created on 10/6/17.
  */
-public class SimpleListImpl implements SimpleList {
+public class SimpleListImpl implements SimpleList, Iterable {
 
 	// TODO: Implement the required methods.
 
+    private Element head;
+    private int size;
+
+    public int size(){
+        return size;
+    }
+
+    public SimpleListImpl(){
+        head = null;
+    }
+
+    public Iterator iterator(){
+        return new SimpleIteratorImpl();
+    }
+
+    public void add(Object item){
+        if(head == null){
+            head = new Element(item);
+        }
+        else{
+            Element current = head;
+            while(current.next != null){
+                current = current.next;
+            }
+            current.next = new Element(item);
+        }
+        size++;
+    }
+
+    private static class Element{
+        private Object item;
+        private Element next;
+
+        Element(Object item){
+            this.item = item;
+            this.next = null;
+        }
+    }
+
+    public class SimpleIteratorImpl implements Iterator<Object>{
+        private Element current = head;
+
+        public boolean hasNext(){
+            return current != null;
+        }
+
+        public Object next(){
+            Object tmp = current.item;
+            current = current.next;
+            return tmp;
+        }
+    }
+
+    public SimpleList filter(SimpleFilter filter){
+        SimpleList result = new SimpleListImpl();
+        for(Object o : this){
+            if(filter.include(o)){
+                result.add(o);
+            }
+        }
+        return result;
+    }
 }
